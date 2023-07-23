4d13586604b8c28bbbad9d86c7fdd6f1e929d58b
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-07-23 14:45:15 +0700

    Initial commit

---
+++ course/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, unique=True, verbose_name='название')
+    preview = models.ImageField(verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(verbose_name='превью', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    video = models.CharField(max_length=150, verbose_name='ссылка на видео', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
---
+++ course/tests.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from course.views import *
+
+router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+
+urlpatterns = [
+    path('lesson/', LessonListView.as_view(), name='lesson_list'),
+    path('lesson/detail/<int:pk>/', LessonDetailAPIView.as_view(), name='lesson_detail'),
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/update/<int_pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('lesson/delete/<int:pk>', LessonDeleteAPIView.as_view(), name='lesson_delete')
+] + router.urls
