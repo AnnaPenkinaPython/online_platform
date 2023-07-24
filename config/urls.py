4a7ac6db7d5d90f42b73ea1c983e60887604e8d9
AnnaPenkinaPython <annapenkina200121@mail.ru>
2023-07-24 13:42:23 +0700

    comma added

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('course.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
     path('users/', include('users.urls')),
     path('payment/', include('payment.urls'))
 ]
